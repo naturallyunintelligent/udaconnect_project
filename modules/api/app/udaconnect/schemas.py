40c6a575ebb0ffa7bd4c04901c0af7f190ac9f89
Darren Steele <darren.steele@five.ai>
2023-10-02 17:34:06 +0100

    strangle location service out to separate service

--- modules/api/app/udaconnect/schemas.py
+++ modules/api/app/udaconnect/schemas.py
@@ -4,17 +4,6 @@ from marshmallow import Schema, fields
 from marshmallow_sqlalchemy.convert import ModelConverter as BaseModelConverter
 
 
-class LocationSchema(Schema):
-    id = fields.Integer()
-    person_id = fields.Integer()
-    longitude = fields.String(attribute="longitude")
-    latitude = fields.String(attribute="latitude")
-    creation_time = fields.DateTime()
-
-    class Meta:
-        model = Location
-
-
 class PersonSchema(Schema):
     id = fields.Integer()
     first_name = fields.String()
---
+++ modules/location_api/app/udaconnect/__init__.py
@@ -0,0 +1,8 @@
+from app.udaconnect.models import Location  # noqa
+from app.udaconnect.schemas import LocationSchema # noqa
+
+
+def register_routes(api, app, root="api"):
+    from app.udaconnect.controllers import location_api as udaconnect_location_api
+
+    api.add_namespace(udaconnect_location_api, path=f"/{root}")
